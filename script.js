03895378d08a4cd1778418c1dbc688985ba98b3e
Ashur19 <iglesiasjota1911@gmail.com>
2023-01-24 11:24:40 +0100

    Card Reverse

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-fetch(' https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/')
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(err => console.log('Solicitud fallida', err))
+let url = 'https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/'
+
+fetch (url) 
+    .then((response) => response.json())
+    .then((data) => {
+        for (i = 0 ; i < data.length; i++) {
+            document.querySelector('.container').innerHTML += `<div class="cards-reverse"><img src="${data[i].cardsReverse.sakuraReverse}"></img></div>`
+        };
+        
+    })
+
+   
