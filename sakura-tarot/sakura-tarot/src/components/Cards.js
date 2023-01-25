5bb37ad36d7e99adae1f4885c5a8af197e2725d0
Ashur19 <iglesiasjota1911@gmail.com>
2023-01-25 15:31:29 +0100

    Randomise

--- sakura-tarot/sakura-tarot/src/components/Cards.js
+++ sakura-tarot/sakura-tarot/src/components/Cards.js
@@ -2,37 +2,36 @@ import React from "react";
 
 
 const Cards = ({ cards = [] }) => {
+  
 
   
   const pulsar = () => {
     this.setState({showText: true})
   };
 
+  
+
+  cards.sort(() => Math.random() - 0.5);
 
   return (
+    
     <div className="row">
       {cards.map((item, index) => (
         <div key={index} className="col">
-          <div className="card" style={{ width: "200px", marginTop: "25px" }}>
-            <img
-              style={{ height: "250px" }}
-              src={item.cardsReverse.sakuraReverse}
-              alt=""
-            />
-            <button
-              style={{ textAlign: "center" }}
-              className="btn btn-dark"
-              onClick={pulsar}
-            >
+          <div className="card">
+            <img id="image" src={item.cardsReverse.sakuraReverse} alt=""/>
+            <button className="btn btn-dark" onClick={pulsar}>
               {item.kanji}
               {/* {this.state.showText && <p>Se muestra esto</p>} */}
-              
             </button>
           </div>
         </div>
       ))}
     </div>
+    
   );
+  
 };
+
 
 export default Cards;
